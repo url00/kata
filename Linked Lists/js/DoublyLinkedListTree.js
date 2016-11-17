f677a7d522c16aa57a4232ab79a645ab3fd20167
Nathan Bergeron <nathanbrgrn@gmail.com>
2016-11-16 18:41:47 -0600

    Clean up pass.

--- Linked Lists/js/DoublyLinkedListTree.js
+++ Linked Lists/js/DoublyLinkedListTree.js
@@ -2,56 +2,66 @@
 
     function Node () {
         this.next = undefined;
-        this.previous = undefined;
+        this.prev = undefined;
         this.data = undefined;
         this.child = undefined;
     }
 
-    function nodeWith(data, child = undefined, next = undefined, preivous = undefined) {
+    function nodeWith(data, child = undefined) {
         var node = new Node();
         node.data = data;
-        node.next = next;
-        node.previous = preivous;
         node.child = child;
         return node;
     }
+    
+    function linkNodes(nodes) {
+        var i = 0;
 
-    Node.prototype.log = function (nodeNum = 0, depth = 0) {
-        console.log(nodeNum + "." + depth + ": " + this.data);
+        while (i + 1 < nodes.length) {
+            nodes[i].next = nodes[i + 1]
+            nodes[i + 1].prev = nodes[i];
+            i++;
+        }
+
+        return nodes[0];
+    }
+
+    Node.prototype.log = function (nodeNumber = 0, depth = 0) {
+        console.log(nodeNumber + "." + depth + ": " + this.data);
 
         if (this.child != null) {
-            this.child.log(nodeNum, depth + 1);
+            this.child.log(nodeNumber, depth + 1);
         }
         
         if (this.next != null) {
-            this.next.log(nodeNum + 1, depth);
+            this.next.log(nodeNumber + 1, depth);
         }
     };    
 
-    Node.prototype.elementToSpan = function () {
-        var newElem = document.createElement("span");
-        newElem.classList.add("element");
-        newElem.innerText = this.data;
+    Node.prototype.toSpan = function () {
+        var span = document.createElement("span");
+        span.classList.add("element");
+        span.innerText = this.data;
 
         if (this.child !== undefined) {
-            newElem.appendChild(this.child.subTreeToUL());
+            span.appendChild(this.child.linkedNodesToUL());
         }
 
-        return newElem;
+        return span;
     }
 
-    Node.prototype.subTreeToUL = function () {
+    Node.prototype.linkedNodesToUL = function () {
         var ul = document.createElement("ul");
 
         var current = this;
         while (current !== undefined) {
-            ul.appendChild(current.elementToSpan());
+            ul.appendChild(current.toSpan());
 
             if (current.next !== undefined) {
-                var newArrow = document.createElement("li");
-                newArrow.classList.add("arrow");
-                newArrow.innerHTML = "&#x2192;";
-                ul.appendChild(newArrow);
+                var arrow = document.createElement("li");
+                arrow.classList.add("arrow");
+                arrow.innerHTML = "&#x2194;";
+                ul.appendChild(arrow);
             }
 
             current = current.next;
@@ -64,25 +74,13 @@
         this.head = undefined;
         this.tail = undefined;
     }
-    
-    function linkNodes(nodes) {
-        var i = 0;
-
-        while (i + 1 < nodes.length) {
-            nodes[i].next = nodes[i + 1]
-            nodes[i + 1].preivous = nodes[i];
-            i++;
-        }
-
-        return nodes[0];
-    }
 
     DoublyLinkedListTree.prototype.generateTreeWithData = function () {
-        var output = new DoublyLinkedListTree();
+        var treeWithData = new DoublyLinkedListTree();
 
-        output.pushNode(nodeWith(1));
+        treeWithData.pushNode(nodeWith(1));
 
-        output.pushNode(
+        treeWithData.pushNode(
             nodeWith(2, linkNodes([
                 nodeWith(2, linkNodes([
                     nodeWith(12, linkNodes([
@@ -94,10 +92,11 @@
             ]))
         );
             
-        output.pushNode(nodeWith(17));
-        output.pushNode(nodeWith(33));
+        treeWithData.pushNode(nodeWith(17));
+
+        treeWithData.pushNode(nodeWith(33));
         
-        output.pushNode(
+        treeWithData.pushNode(
             nodeWith(5, linkNodes([
                 nodeWith(6),
                 nodeWith(25, nodeWith(8)),
@@ -105,33 +104,9 @@
             ]))
         );
 
-        return output;
+        return treeWithData;
     }
-
-    DoublyLinkedListTree.prototype.generateTreeWithMyData = function () {
-        var mainTree = new DoublyLinkedListTree();
-        mainTree.head = new Node();
-        mainTree.head.data = 0;
-        var current = mainTree.head;
-        for (i = 1; i < 4; i++) {
-            var newNode = new Node();
-            newNode.previous = current;
-            newNode.data = i;
-
-            if (i % 2 === 1) {
-                newNode.child = new Node();
-                newNode.child.data = i * 10;
-                newNode.child.next = new Node();
-                newNode.child.next.data = i + 1 * 100;
-            }
-
-            current.next = newNode;
-            current = current.next;
-        }
-
-        return mainTree;
-    };
-
+    
     DoublyLinkedListTree.prototype.pushNode = function (node) {
         if (this.head === undefined) {
             this.head = node;
@@ -140,7 +115,7 @@
         }
 
         node.next = this.head;
-        this.head.previous = node;
+        this.head.prev = node;
         this.head = node;
     }
 
@@ -164,7 +139,7 @@
     };
 
     DoublyLinkedListTree.prototype.toUL = function () {
-        return this.head.subTreeToUL();
+        return this.head.linkedNodesToUL();
     };
 
     window.DoublyLinkedListTree = DoublyLinkedListTree;
