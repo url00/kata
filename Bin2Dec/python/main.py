10784ca4ab53aaf17e59ce8172ac2a15b46ed046
Nathan Bergeron <nathan.b@awsys.com>
2020-11-02 00:09:21 -0600

    Added features.

--- Bin2Dec/python/main.py
+++ Bin2Dec/python/main.py
@@ -3,31 +3,44 @@ import blessed
 def tprint(x):
     print(x, end='')
 
+def draw_label(term, label, y, other_labels):
+    max_length = max([len(x) for x in other_labels])
+    tprint(term.move_xy(max_length - len(label), y) + label)
+
+def draw_static(term):
+    input_field_label =  "Binary input:"
+    result_field_label = "Decimal result:"
+    all_labels = (input_field_label, result_field_label)
+    all_labels_max_length = max([len(x) for x in all_labels])
+    data_x_start = all_labels_max_length + 1
+
+    tprint(term.home + term.clear)
+    tprint(term.underline)
+    draw_label(term, input_field_label, 0, all_labels)
+    draw_label(term, result_field_label, 2, all_labels)
+    tprint(term.no_underline)
+    return data_x_start
+
 def main():
-    bin_input = [0 for i in range(8)]
+    max_length = 32
+    bin_input = [0 for i in range(max_length)]
     dec_output = 0
     term = blessed.Terminal()
     bin_input_i = 0
 
     with term.fullscreen(), term.cbreak():
-        input_field_label =  "Binary input (max 8 chars):"
-        result_field_label = "Result:"
-        tprint(term.home + term.clear)
-        tprint(term.underline)
-        tprint(input_field_label)
-        tprint(term.move_xy(len(input_field_label) - len(result_field_label), 1) + result_field_label)
-        tprint(term.no_underline)
+        data_x_start = draw_static(term)
         
         while True:
             dec_output = 0
             for i, x in enumerate(bin_input):
                 if x == 1:
-                    dec_output += 2 ** (7 - i)
+                    dec_output += 2 ** (max_length - 1 - i)
 
-            tprint(term.move_xy(len(input_field_label) + 1, 0) + ''.join([str(x) for x in bin_input]))
-            tprint(term.move_xy(len(input_field_label) + 1, 1) + term.clear_eol + str(dec_output))
+            tprint(term.move_xy(data_x_start, 0) + ''.join([str(x) for x in bin_input]))
+            tprint(term.move_xy(data_x_start, 2) + term.clear_eol + str(dec_output))
 
-            tprint(term.move_xy(len(input_field_label) + 1 + bin_input_i, 0))
+            tprint(term.move_xy(data_x_start + bin_input_i, 0))
 
             raw_key = term.inkey()
             key = raw_key.name
@@ -38,14 +51,24 @@ def main():
                 break
             elif key in ('1', '0'):
                 bin_input[bin_input_i] = int(key)
-                bin_input_i = (bin_input_i + 1) % 8
+                bin_input_i = (bin_input_i + 1) % max_length
             elif key == "KEY_LEFT":
                 bin_input_i = max(0, bin_input_i - 1)
             elif key == "KEY_RIGHT":
-                bin_input_i = min(7, bin_input_i + 1)
+                bin_input_i = min(max_length - 1, bin_input_i + 1)
+            elif key == '3':
+                max_length = 32
+                data_x_start = draw_static(term)
+                bin_input = [0 for i in range(max_length)]
+                bin_input_i = 0
+            elif key == '6':
+                max_length = 64
+                data_x_start = draw_static(term)
+                bin_input = [0 for i in range(max_length)]
+                bin_input_i = 0
             else:
                 continue
-            
+
     return 0
 
 exit(main())
