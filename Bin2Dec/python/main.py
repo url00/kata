5d00914f241451eb7794f669f4e759beeda916a8
Nathan Bergeron <nathan.b@awsys.com>
2020-11-01 23:52:36 -0600

    bin2dec done

--- Bin2Dec/python/main.py
+++ Bin2Dec/python/main.py
@@ -1,40 +1,51 @@
 import blessed
 
-def get_new_current_bin_input_from_key(current_bin_input, key):
-    return current_bin_input + key
-
 def tprint(x):
     print(x, end='')
 
 def main():
-    current_bin_input = ""
-    current_dec_output = "outupt"
+    bin_input = [0 for i in range(8)]
+    dec_output = 0
     term = blessed.Terminal()
+    bin_input_i = 0
 
-    with term.cbreak():
+    with term.fullscreen(), term.cbreak():
+        input_field_label =  "Binary input (max 8 chars):"
+        result_field_label = "Result:"
+        tprint(term.home + term.clear)
+        tprint(term.underline)
+        tprint(input_field_label)
+        tprint(term.move_xy(len(input_field_label) - len(result_field_label), 1) + result_field_label)
+        tprint(term.no_underline)
+        
         while True:
-            current_dec_output = str(term.get_location())
-            input_field_label =  "Binary input (max 8 chars):"
-            result_field_label = "Result:"
-            tprint(term.home + term.clear)
-            tprint(term.underline)
-            tprint(input_field_label)
-            tprint(term.home + term.move_down())
-            tprint(term.move_xy(len(input_field_label) - len(result_field_label), 1))
-            tprint(term.no_underline)
-            tprint(term.move_xy(len(input_field_label) + 1, 0) + current_bin_input)
-            tprint(term.move_xy(len(input_field_label) + 1, 1) + current_dec_output)
-            tprint(term.move_xy(len(input_field_label) + 1 + len(current_bin_input), 0))
+            dec_output = 0
+            for i, x in enumerate(bin_input):
+                if x == 1:
+                    dec_output += 2 ** (7 - i)
 
+            tprint(term.move_xy(len(input_field_label) + 1, 0) + ''.join([str(x) for x in bin_input]))
+            tprint(term.move_xy(len(input_field_label) + 1, 1) + term.clear_eol + str(dec_output))
 
-            key = term.inkey()
+            tprint(term.move_xy(len(input_field_label) + 1 + bin_input_i, 0))
+
+            raw_key = term.inkey()
+            key = raw_key.name
+            if key == None:
+                key = raw_key
+
             if key in ('q', 'KEY_ESCAPE'):
                 break
+            elif key in ('1', '0'):
+                bin_input[bin_input_i] = int(key)
+                bin_input_i = (bin_input_i + 1) % 8
+            elif key == "KEY_LEFT":
+                bin_input_i = max(0, bin_input_i - 1)
+            elif key == "KEY_RIGHT":
+                bin_input_i = min(7, bin_input_i + 1)
             else:
-                current_bin_input =  get_new_current_bin_input_from_key(current_bin_input, key)
-
-
-
+                continue
+            
     return 0
 
 exit(main())
